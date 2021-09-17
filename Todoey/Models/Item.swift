42044868c4b6d8bdc1377bc5293a40940b5c1d6a
Yoshinobu Hara <yoshihara800@gmail.com>
2021-09-17 16:17:17 -0700

    Complete 268 Realm logic

--- Todoey/Models/Item.swift
+++ Todoey/Models/Item.swift
@@ -13,7 +13,6 @@ class Item: Object {
     @objc dynamic var title: String = ""
     @objc dynamic var done: Bool = false
     @objc dynamic var dateCreated: Date = Date()
-
     //Inverse relationship with Category
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
